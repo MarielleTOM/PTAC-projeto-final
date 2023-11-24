a00fc9caf87e34dd445cd00761bed52f5e5ca2f9
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-11-24 15:47:18 -0300

    24/11
    add o git ignore, add novas pastas e arquivos, e comecei a mexer no home, colocando a navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Home(){
+
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <div>
+        <h1>Vogue Beauty Secrets</h1>
+        <div className="navbar">
+             <Link to="/">Home</Link>
+             <Link to="/EmDestaque">Em Destaques</Link>
+             <Link to="/NovoVideo">Novo Video</Link>
+        </div>
+        </div>
      );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
+import NovoVideo from './Todo/ToDo';
+import EmDestaque from './detalhe/detalhe';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path='/NovoVideo' element={<NovoVideo />}></Route>
+        <Route path='/EmDestaque' element={<EmDestaque />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
