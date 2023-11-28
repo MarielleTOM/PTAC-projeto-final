b25a4a2241789af23e0a2ba7bdc96966afc1ac0f
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-11-28 08:45:42 -0300

    28/11-2
    Comecei adicionar uma navbar no header

---
+++ Detalhe/Detalhe.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+import Card from "../Componentes/Card";
+
+export default function Detalhe(){
+
+}
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../Componentes/Header";
 export default function Home(){
 
     return(
         <div>
-        <h1>Vogue Beauty Secrets</h1>
-        <div className="navbar">
-             <Link to="/">Home</Link>
-             <Link to="/EmDestaque">Em Destaques</Link>
-             <Link to="/NovoVideo">Novo Video</Link>
-        </div>
+          <Header/>
         </div>
      );
 }
