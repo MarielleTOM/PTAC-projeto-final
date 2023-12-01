03c704a5079459d8a7bf7cfd6e8f3a8819e0799c
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-12-01 15:31:55 -0300

    01/12
    Mexi no NovoVideo, criei as consts dos atributos, criei a const para salvar, e no return coloquei os inputs e para no site. o código do cadastro eu peguei do trabalho anterior e arrumei para o que eu desejava.

--- src/NovoVideo/NovoVideo.jsx
+++ src/NovoVideo/NovoVideo.jsx
@@ -2,68 +2,71 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Componentes/Header";
-export default function NovoVideo(){
+export default function NovoVideo() {
 
-    const listaLocalStorage= JSON.parse(localStorage.getItem("Lista"));
-    const [produto, setProduto]= useState("");
-    const [marca, setMarca]= useState("");
-    const [preco, setPreco]= useState("");
-    const [lista,setLista]= useState(listaLocalStorage || []);
-    const [img,setImg]= useState("");
-    const [id,setId] = useState(listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1);
+    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
+    const [lista, setLista] = useState(listaLocalStorage || []);
+    const [id, setId] = useState(listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1);
+    const [nomeVideo, setNomeVideo] = useState("");
+    const [url, setUrl] = useState("");
+    const [famoso, setFamoso] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [produtos, setProdutos] = useState("");
 
-    useEffect(()=> {localStorage.setItem("Lista", JSON.stringify(lista))},[lista]);
-    
+    useEffect(() => { localStorage.setItem("Lista", JSON.stringify(lista)) }, [lista]);
+
     const salvar = (e) => {
         e.preventDefault();
-        setLista([...lista,{
-            produto: produto, marca: marca, preco: preco, id:id, img: img
+        setLista([...lista, {
+            id: id, nomeVideo: nomeVideo, url: url, famoso: famoso, descricao: descricao, produtos: produtos
         }]);
         setId(id + 1);
-        setProduto("");
-        setMarca("");
-        setPreco("");
-        setImg("");
+        setNomeVideo("");
+        setUrl("");
+        setFamoso("");
+        setDescricao("");
+        setProdutos("");
 
     }
 
-    return(
+    return (
         <div>
-          <Header/>
-          <p>{produto}, {marca}, {preco}</p>
+            <Header />
 
             <form onSubmit={salvar}>
-                <p>Produto:</p>
-                <input class="form-control" type="text" placeholder="Produto" aria-label="default input example" value={produto}
-                    onChange={(e) => setProduto(e.target.value)}/>
+                <p>Nome do Video:</p>
+                <input type="text" placeholder="NomeVideo" aria-label="default input example" value={nomeVideo}
+                    onChange={(e) => setNomeVideo(e.target.value)} />
 
-                <p>Marca:</p>
-                <input class="form-control" type="text" placeholder="Marca" aria-label="default input example" value={marca}
-                    onChange={(e) => setMarca(e.target.value)}/>
+                <p>Famoso:</p>
+                <input type="text" placeholder="Famoso" aria-label="default input example" value={famoso}
+                    onChange={(e) => setFamoso(e.target.value)} />
 
-                <p>Preço:</p>
-                <input class="form-control" type="number" placeholder="Preço" aria-label="default input example" value={preco}
-                    onChange={(e) => setPreco(e.target.value)}/>
-                
-                <p>Imagem:</p>
-                <input class="form-control" type="text" placeholder="Link da imagem" aria-label="default input example" value={img}
-                    onChange={(e) => setImg(e.target.value)}/>
+                <p>Descrição:</p>
+                <input type="text" placeholder="Descicao" aria-label="default input example" value={descricao}
+                    onChange={(e) => setDescicao(e.target.value)} />
 
-                <br/>
+                <p>Prosutos usados:</p>
+                <input type="text" placeholder="Produtos" aria-label="default input example" value={produtos}
+                    onChange={(e) => setProdutos(e.target.value)} />
+
+                <br />
 
                 <button class="btn btn-outline-dark">ADD</button>
             </form>
-           {lista.map((ativ)=> 
-                <div key= {ativ.id}>
+
+            {lista.map((ativ) =>
+                <div key={ativ.id}>
                     <Link to={'/detalhe/${ativ.id}'}>
-                    <p>{ativ.atividade}</p>
+                        <p>{ativ.nomeVideo}</p>
                     </Link>
-                    <p><img src={ativ.img} alt=""/></p>
-                    <p>Produto: {ativ.produto}, Marca: {ativ.marca}, R$: {ativ.preco} </p>
-                     <br/>
-                    <button class="btn btn-outline-dark" onClick={() => remover (ativ.id)}>Remover</button>
+                    <div>
+                        <p> Famoso: {ativ.famoso}, Descrição: {ativ.descricao}, Produtos usados: {ativ.produtos} </p>
+                        <br />
+                    </div>
                 </div>
             )}
+
         </div>
-     );
+    );
 }
