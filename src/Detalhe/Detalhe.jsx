1e0e52b6e6cd3bd4723e9a7751b94b46e5fc0254
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-12-08 10:30:48 -0300

    08/12 - 1
    Mexi no detalhe para aparecer apenas um video e os seus detalhes

--- src/Detalhe/Detalhe.jsx
+++ src/Detalhe/Detalhe.jsx
@@ -1,20 +1,25 @@
 import { useParams } from "react-router-dom";
 import Card from "../Componentes/Card";
 
-export default function Detalhe(){
-        const { id }= useParams();
-        const lista = JSON.parse( localStorage.getItem("Lista"));
-        
-        const atividade = lista.filter((objeto) => {
-            if(objeto.id == id) {
-                return objeto;
-            }
-            return null;
-        });
-    
-        console.log(atividade[0]);
-    
-        return (
-            < Card atividade={atividade[0]} />
-        );
+export default function Detalhe() {
+    const { id } = useParams();
+    const lista = JSON.parse(localStorage.getItem("Lista"));
+
+    const atividade = lista.filter((objeto) => {
+        if (objeto.id == id) {
+            return objeto;
+        }
+        return null;
+    });
+
+    return (
+    atividade.map((video) =>
+    <div key= {video.id}>
+        <p>{video.nomeVideo}</p>
+        <p>{video.famoso}</p>
+        <p>{video.descricao}</p>
+        <p>{video.produtos}</p>
+    </div>
+    )
+)
 }
