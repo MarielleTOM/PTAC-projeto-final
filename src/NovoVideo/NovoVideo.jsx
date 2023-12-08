7f3204ad121300157983633cde86619bf6fabc5f
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-12-08 15:35:01 -0300

    08/12 - 5
    Não tava indo pra home quando cadastrava o vídeo, mas so add umas coisinhas q eu esqueci

--- src/NovoVideo/NovoVideo.jsx
+++ src/NovoVideo/NovoVideo.jsx
@@ -16,7 +16,7 @@ export default function NovoVideo() {
     useEffect(() => { localStorage.setItem("Lista", JSON.stringify(lista)) }, [lista]);
     console.log(lista)
 
-    const navigate = useNavigate
+    const navigate = useNavigate();
     
     const salvar = (e) => {
         e.preventDefault();
