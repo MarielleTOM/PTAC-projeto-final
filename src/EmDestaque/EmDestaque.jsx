111d9f2b4a2f81ed0c03869e56e1e1d8827e85ba
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-12-08 15:20:19 -0300

    08/12 - 4
    Criei as consts uma pra chamar a lista, outra pra cortar ela, assim pegando os vídeos q eu quero, e depois coloquei o card com o negócio para definir os vídeos a ser pegados

--- src/EmDestaque/EmDestaque.jsx
+++ src/EmDestaque/EmDestaque.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Componentes/Header";
-export default function EmDestaque(){
+import Card from "../Componentes/Card"
+export default function EmDestaque() {
+  const destaques = JSON.parse(localStorage.getItem("Lista"));
+  const aparecerDestaque = destaques.slice(-3)
+console.log(aparecerDestaque)
+  return (
+    <div>
+      <Header />
+      <Card lista={aparecerDestaque} />
 
-    return(
-        <div>
-          <Header/>
-          <h1></h1>
-        </div>
-     );
+    </div>
+  );
 }
